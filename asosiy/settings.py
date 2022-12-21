9050b82eafe306033dc533726702bc254239703c
hfazliddin98 <93676290+hfazliddin98@users.noreply.github.com>
2022-12-21 15:46:04 +0500

    21.12.2022 15:45

--- asosiy/settings.py
+++ asosiy/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = env.str("SECRET_KEY")
-SECRET_KEY='django-insecure-vptmbjpy(@)jj&ilxa0s$*pz=&tz!3omrej33r)&&a+a^(hr_8'
+SECRET_KEY = env.str("SECRET_KEY")
+#SECRET_KEY='django-insecure-vptmbjpy(@)jj&ilxa0s$*pz=&tz!3omrej33r)&&a+a^(hr_8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
